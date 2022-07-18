9a591fcfa1aecc3e94556a7b57a5d3c1f44708be
Tyler Durkota <tdurk93@gmail.com>
2022-07-17 22:36:01 -0700

    Add ability to decode base64 back to its input

--- bsixfour.go
+++ bsixfour.go
@@ -8,18 +8,24 @@ import (
 	"strings"
 )
 
-const sextetBitMask uint8 = 1<<6 - 1 // 0b00111111
+const sextetBitMask byte = 1<<6 - 1 // 0b00111111
 
 func main() {
-	base64Output := Encode(os.Stdin)
-	fmt.Println(base64Output)
+	// TODO add ability to either encode or decode from command line
+	// base64Output := Encode(os.Stdin)
+	// fmt.Println(base64Output)
+	originalData := Decode(os.Stdin)
+	fmt.Print(originalData)
+	// printing final newline using stderr
+	// so it doesn't corrupt output when piped to other applications.
+	// Eventually might include a flag to make this behaivor optional
+	fmt.Fprintf(os.Stderr, "\n")
 }
 
 func Encode(reader io.Reader) string {
 	// note that this is currently not a great implementation
-	// because it stores the entire contents of the base64
-	// string in memory. I will improve with goroutines/channelss
-	// at a future date.
+	// because it stores the entire contents of the base64 string in memory
+	// I will improve with goroutines/channels at a future date.
 	lookupTable := buildBase64EncoderLookupTable()
 	stringBuilder := new(strings.Builder)
 
@@ -43,7 +49,7 @@ func Encode(reader io.Reader) string {
 		var outBuf [4]rune
 		for n := 0; n < len(outBuf); n++ {
 			// starting with most-significant/leftmost sextet, use bitmask to extract sextets & lookup table to get ascii character
-			outBuf[n] = lookupTable[uint8(threeByteChunk>>(18-n*6))&sextetBitMask]
+			outBuf[n] = lookupTable[byte(threeByteChunk>>(18-n*6))&sextetBitMask]
 		}
 
 		// at the end of the input, we might have to add padding
@@ -58,21 +64,79 @@ func Encode(reader io.Reader) string {
 	return stringBuilder.String()
 }
 
-func buildBase64EncoderLookupTable() map[uint8]rune {
+func buildBase64EncoderLookupTable() map[byte]rune {
 	const capitalAOffset = 65
 	const lowercaseAOffset = capitalAOffset | 1<<5 // (97)
 	const zeroCharOffset = 48
-	lookupTable := make(map[uint8]rune)
+	lookupTable := make(map[byte]rune)
 	for n := 0; n < 26; n++ {
-		lookupTable[uint8(n)] = rune(n + capitalAOffset)
+		lookupTable[byte(n)] = rune(n + capitalAOffset)
 	}
 	for n := 26; n < 52; n++ {
-		lookupTable[uint8(n)] = rune(n + lowercaseAOffset - 26)
+		lookupTable[byte(n)] = rune(n + lowercaseAOffset - 26)
 	}
 	for n := 52; n < 62; n++ {
-		lookupTable[uint8(n)] = rune(n + zeroCharOffset - 52)
+		lookupTable[byte(n)] = rune(n + zeroCharOffset - 52)
 	}
 	lookupTable[62] = '+'
 	lookupTable[63] = '/'
+	return lookupTable
+}
+
+func Decode(reader io.Reader) string {
+	decodeLookupTable := buildBase64DecoderLookupTable()
+	stringBuilder := new(strings.Builder)
+
+	for {
+		var inBuf [4]byte
+		numBytes, err := reader.Read(inBuf[:]) // read in 4 characters/bytes at a time
+
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				if numBytes != 0 {
+					// Input length was not a multiple of 4.
+					// This tool only supports base64 with padding.
+					panic("Input length not valid")
+				}
+				break
+			} else { // unexpected
+				panic(err)
+			}
+		}
+
+		numUsedBytes := 3
+		// construct 24-bit chunk that we can break into 3 bytes
+		var fourCharacterChunk uint32 = 0
+		for n := 0; n < len(inBuf); n++ {
+			// first perform padding check so we can avoid using the lookup table
+			// on padding characters where there isn't technically an output mapping
+			if n >= 2 && rune(inBuf[n]) == '=' { // only check 3rd & 4th characters for padding
+				numUsedBytes = n - 1
+				break
+			}
+			fourCharacterChunk = fourCharacterChunk | uint32(decodeLookupTable[rune(inBuf[n])])<<(6*(3-n))
+		}
+		outBuf := [3]byte{byte(fourCharacterChunk >> 16), byte(fourCharacterChunk >> 8), byte(fourCharacterChunk)}
+		stringBuilder.WriteString(string(outBuf[0:numUsedBytes]))
+	}
+	return stringBuilder.String()
+}
+
+func buildBase64DecoderLookupTable() map[rune]byte {
+	const capitalAOffset = 65
+	const lowercaseAOffset = capitalAOffset | 1<<5 // (97)
+	const zeroCharOffset = 48
+	lookupTable := make(map[rune]byte)
+	for n := 0; n < 26; n++ {
+		lookupTable[rune(n+capitalAOffset)] = byte(n)
+	}
+	for n := 26; n < 52; n++ {
+		lookupTable[rune(n+lowercaseAOffset-26)] = byte(n)
+	}
+	for n := 52; n < 62; n++ {
+		lookupTable[rune(n+zeroCharOffset-52)] = byte(n)
+	}
+	lookupTable['+'] = 62
+	lookupTable['/'] = 63
 	return lookupTable
 }
